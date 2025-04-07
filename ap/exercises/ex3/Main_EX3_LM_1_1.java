ac7a852427604b9dbcac04293a07aa2b4cd2f3d4
Katayoun4121 <@catayoun.adi@gmail.com>
2025-04-07 10:58:40 -0700

    EX3_LM_1_1

--- ap/exercises/ex3/Main_EX3_LM_1_1.java
+++ ap/exercises/ex3/Main_EX3_LM_1_1.java
@@ -1,15 +1,16 @@
 package ap.exercises.ex3;
 
-class Book{
+class Book {
     private String title;
     private String author;
     private String numPage;
     private String year;
-    public Book(String title, String author, String numPage,String year){
-        this.title=title;
-        this.author=author;
-        this.numPage=numPage;
-        this.year=year;
+
+    public Book(String title, String author, String numPage, String year) {
+        this.title = title;
+        this.author = author;
+        this.numPage = numPage;
+        this.year = year;
     }
 
     public void setTitle(String title) {
@@ -27,26 +28,30 @@ class Book{
     public void setYear(String year) {
         this.year = year;
     }
-    public void displayInfo(){
-        System.out.println("book name:"+title);
-        System.out.println("author name:"+author);
-        System.out.println("number of pages:"+numPage);
-        System.out.println("publication year:"+year);
+
+    public void displayInfo() {
+        System.out.println("book name:" + title);
+        System.out.println("author name:" + author);
+        System.out.println("number of pages:" + numPage);
+        System.out.println("publication year:" + year);
     }
 }
-class Student{
+
+class Student {
     private String fullName;
     private String studentId;
     private String major;
-    public Student (String fullName,String studentId,String major){
-        this.fullName=fullName;
-        this.studentId=studentId;
-        this.major=major;
+
+    public Student(String fullName, String studentId, String major) {
+        this.fullName = fullName;
+        this.studentId = studentId;
+        this.major = major;
     }
 
     public void setFullName(String fullName) {
         this.fullName = fullName;
     }
+
 
     public void setStudentId(String studentId) {
         this.studentId = studentId;
@@ -55,29 +60,39 @@ class Student{
     public void setMajor(String major) {
         this.major = major;
     }
-    public void displayInfo(){
-        System.out.println("student's full name:"+fullName);
-        System.out.println("studentId:"+studentId);
-        System.out.println("student's major:"+major);
+
+    public void displayInfo() {
+        System.out.println("student's full name:" + fullName);
+        System.out.println("studentId:" + studentId);
+        System.out.println("student's major:" + major);
     }
 }
 
 public class Main_EX3_LM_1_1 {
-    public static void main(String[] args){
-        Book book1=new Book("sinoohe's bio","sinoohe","10000","1899");
-        Book book2=new Book("hassani","ali","15","1999");
+    public static void main(String[] args) {
+        Book book1 = new Book("sinoohe's bio", "sinoohe", "10000", "1899");
+        Book book2 = new Book("hassani", "ali", "15", "1999");
+        Student student1 = new Student("katayoun akhoundi", "403463111", "computer science");
+        Student student2 = new Student("saber abar", "403463112", "electric");
         System.out.println("--------------------------------");
+        System.out.println("main information: ");
+
         book1.displayInfo();
         System.out.println("--------------------------------");
         book2.displayInfo();
-        System.out.println("--------------------------------");
-        Student student1=new Student("katayoun akhoundi","403463111","computer science");
-        Student student2=new Student("saber abar","403463112","electric");
         System.out.println("--------------------------------");
         student1.displayInfo();
         System.out.println("--------------------------------");
         student2.displayInfo();
         System.out.println("--------------------------------");
+        student1.setStudentId("402575111");
+        book1.setYear("2017");
+        System.out.println("--------------------------------");
+        System.out.println("edited information.");
+        book1.displayInfo();
+        System.out.println("--------------------------------");
+        student1.displayInfo();
+
     }
 }
 
